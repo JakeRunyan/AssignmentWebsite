e088a36b94d16320bf9eab781d8ea7c05402a2c9
Jake Runyan <43076688+Jtmonkeyboy@users.noreply.github.com>
2023-11-02 17:53:25 -0600

    Created real-time timer

--- websites/TulsaNewYear/scripts/tny_script.js
+++ websites/TulsaNewYear/scripts/tny_script.js
@@ -7,4 +7,26 @@
 
 */
 
-window.alert("Welcome to Tulsa");
+//window.alert("Welcome to Tulsa");
+
+var currentDay = new Date();
+var dateStr = currentDay.toLocaleDateString();
+var timeStr = currentDay.toLocaleTimeString();
+
+var dateDiv = document.getElementById("dateNow");
+dateDiv.innerHTML = dateStr + "<br>" + timeStr;
+
+var newYear = new Date("January 1, 2018");
+var nextYear = currentDay.getFullYear() + 1;
+newYear.setFullYear(nextYear);
+
+var daysLeft = (newYear - currentDay) / (1000*60*60*24);
+var hoursLeft = ((daysLeft) - Math.floor(daysLeft)) * 24;
+var minsLeft = ((hoursLeft) - Math.floor(hoursLeft)) * 60;
+var secondsLeft = ((minsLeft) - Math.floor(minsLeft)) * 60;
+
+document.getElementById("days").textContent = Math.floor(daysLeft);
+document.getElementById("hrs").textContent = Math.floor(hoursLeft);
+document.getElementById("mins").textContent = Math.floor(minsLeft);
+document.getElementById("secs").textContent = Math.floor(secondsLeft);
+
