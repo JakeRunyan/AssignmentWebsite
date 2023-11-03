b800161d5727cef72718885a871dfbef979f6428
Jake Runyan <43076688+Jtmonkeyboy@users.noreply.github.com>
2023-11-02 19:22:10 -0600

    fixed the connection to the Js file

--- websites/StarDust/scripts/sd_mapper.js
+++ websites/StarDust/scripts/sd_mapper.js
@@ -1,7 +1,13 @@
+"use strict";
 /*
-
    Planisphere Script
    Author: Jake Runyan
    Date: 11/2/23
 */
 
+var thisTime = new Date("February 3, 2018 3:15:28 AM");
+var timeStr = thisTime.toLocaleString();
+
+var timeDiv = document.getElementById("timeStamp");
+
+timeDiv.innerHTML = timeStr;
