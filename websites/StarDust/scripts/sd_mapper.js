cb4498f765d8f9d366e7af2bc7c5d893aee52873
Jake Runyan <43076688+Jtmonkeyboy@users.noreply.github.com>
2023-11-02 19:46:51 -0600

    Added comments

--- websites/StarDust/scripts/sd_mapper.js
+++ websites/StarDust/scripts/sd_mapper.js
@@ -5,9 +5,16 @@
    Date: 11/2/23
 */
 
+// Create a new date and store it in a string
 var thisTime = new Date("February 3, 2018 3:15:28 AM");
 var timeStr = thisTime.toLocaleString();
 
-var timeDiv = document.getElementById("timeStamp");
+// This changes a string in the actual HTML
+document.getElementById("timeStamp").innerHTML = timeStr;
 
-timeDiv.innerHTML = timeStr;
+// This extracts the hours and month and calculates map number
+var thisHour = thisTime.getHours();
+var thisMonth = thisTime.getMonth();
+var mapNum = (2*thisMonth + thisHour) % 24;
+
+var imgStr = "<img src='sd_skyMap.png' />";
