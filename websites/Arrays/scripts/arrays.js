7ff114627bbb1ff6bc0d25945a3bfffbc28037de
Jake Runyan <43076688+Jtmonkeyboy@users.noreply.github.com>
2023-11-10 14:41:29 -0700

    Created needed arrays

---
+++ websites/Arrays/scripts/arrays.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*
+    Author: Jake Runyan
+    Date: 11/9/23
+*/
+
+var family = ["Erin", "Nick", "Luke", "Deanna", "Todd", "Cory", "Blanca", "Sammy", "Lexi", "Katie"];
+
+var relationship = ["Wife", "Brother", "Brother", "Mom", "Dad", "Step-Dad", "Step-Mom", "Half-Brother", "Half-Sister", "Mother-In-Law"];
