f38d63d88ef74cde49c24edd550fdcfd27e3f933
Jake Runyan <43076688+Jtmonkeyboy@users.noreply.github.com>
2023-11-13 18:35:28 -0700

    Finished the javascript for this assignment

--- websites/JPF_Hitori/scripts/jpf_hitori.js
+++ websites/JPF_Hitori/scripts/jpf_hitori.js
@@ -5,8 +5,8 @@
    Tutorial 11
    Review Assignment
 
-   Author: 
-   Date:   
+   Author: Jake Runyan
+   Date: 11/13
 
    Global Variables
    ================
@@ -21,54 +21,142 @@
       Run when the web page is loaded; displays puzzle 1
       and loads the event handlers for the web page buttons.
    */
-  window.init; 
-  
-   function init() {
+   window.onload = startUp();
+   
+   var allCells;
+
+   function startUp() {
+      document.getElementById("puzzleTitle").innerHTML = "Puzzle 1";
+
+      document.getElementById("puzzle").innerHTML = drawHitori(hitori1Numbers, hitori1Blocks, hitori1Rating);
+
+      // Add event handlers for the 3 puzzle buttons
+      var puzzleButtons = document.getElementsByClassName("puzzles");
+      for (var i = 0; i < puzzleButtons.length; i++)
+         puzzleButtons[i].onclick = switchPuzzle;
+
+      setupPuzzle();
+
+      // Event handler to check the puzzle if it is finished
+      document.getElementById("check").addEventListener("click", findErrors);
+      // Event handler to show the solution
+      document.getElementById("solve").addEventListener("click", showSolution);
 
    }
+
+
    /*
    setupPuzzle()
       Sets up a new puzzle, adding the event handlers for
       every puzzle cell.      
    */
+   function setupPuzzle() {
+      allCells = document.querySelectorAll("table#hitoriGrid td");
+
+      // Set the initial color to be white
+      for (var i = 0; i < allCells.length; i++) {
+         allCells[i].style.backgroundColor = "white";
+         allCells[i].style.color = "black";
+         allCells[i].style.borderRadius = "0";
+
+         document.addEventListener("mousedown", 
+            function(e) {
+               e.preventDefault();
+               if (e.shiftKey) {
+                  allCells[i].style.backgroundColor = "white";
+                  allCells[i].style.color = "black";
+                  allCells[i].style.borderRadius = "0";
+               }
+               else if (e.altKey) {
+                  allCells[i].style.backgroundColor = "black";
+                  allCells[i].style.color = "white";
+                  allCells[i].style.borderRadius = "0";
+               }
+               else {
+                  allCells[i].style.backgroundColor = "rgb(101, 101, 101)";
+                  allCells[i].style.color = "white";
+                  allCells[i].style.borderRadius = "50%";
+               }
+            }
+         );
+
+         // Event Listener that listens for event down
+         document.addEventListener("mouseover",
+            function(e) {
+               if (e.shiftKey) {
+                  allCells[i].style.cursor = "url(images/jpf_eraser.png), alias";
+               }
+               else if (e.altKey) {
+                  allCells[i].style.cursor = "url(images/jpf_block.png), cell";
+               }
+               else {
+                  allCells[i].style.cursor = "url(images/jpf_pencil.png), pointer";
+               }
+            }
+         );
+
+         // Event listener for the mouse up event
+         document.addEventListener("mouseup", checkSolution());
+      }
+   }
+
+
    /*
    switchPuzzle(e)
       Swaps one puzzle for another based on the button being clicked
       by the user. Confirms the change before swapping in the
       new puzzle.
    */
+   function switchPuzzle(e) {
+      if (confirm("Are you sure? You will lose all of your work on the puzzle.")) {
+         var puzzleId = e.target.id;
+         // Changing the puzzle title
+         document.getElementById("puzzleTitle").innerHTML = e.target.value;
+
+         switch(puzzleId) {
+            case "puzzle1":
+               document.getElementById("puzzle").innerHTML = drawHitori(hitori1Numbers, hitori1Blocks, hitori1Rating);
+               break;
+            case "puzzle2":
+               document.getElementById("puzzle").innerHTML = drawHitori(hitori2Numbers, hitori2Blocks, hitori2Rating);
+               break;
+            case "puzzle3":
+               document.getElementById("puzzle").innerHTML = drawHitori(hitori3Numbers, hitori3Blocks, hitori3Rating);
+               break;
+         }
+         setupPuzzle();
+      }
+   }
+
+
    /*
    findErrors()
       Highlights the errors in the Hitori puzzle in a red font.
    */
-   /*   
-   showSolution()
-      Shows the solution to the Hitori puzzle
-   */
-   /* 
-   checkSolution()
-      Checks the current user's puzzle to verify whether it contains
-      the complete and correct solution.
-   */
-   /*
-   drawHitori(numbers, blocks, rating)
-      Returns a text string of the HTML code to
-      display a Hitori puzzle table based on the contents of
-      the numbers, blocks, and rating parameters.
-	*/
+   function findErrors() {
+      for (var i = 0; i < allCells.length; i++) {
+         if (allCells[i].className === "blocks" && allCells[i].style.backgroundColor === "rgb(101, 101, 100)" ||
+         allCells[i].className === "circles" && allCells[i].style.backgroundColor === "black") 
+            allCells[i].style.color = "red";
+      }
+
+      setTimeout(function() {
+         // Change pink cells to white red cells back gray
+         for (var i = 0; i < allCells.length; i++)
+            if (allCellsCells[i].style.backgroundColor === "red")
+               allCells[i].style.backgroundColor = "white";
+      }, 100);
+   }
 
 
-
-
-
-
-
-
-
-
-         
 /* ================================================================= */
 
+
+/* 
+   checkSolution()
+   Checks the current user's puzzle to verify whether it contains
+   the complete and correct solution.
+*/
 function checkSolution() {
    /* Set the initial solved state of the puzzle to true */
    var solved = true;
@@ -93,6 +181,11 @@ function checkSolution() {
    if (solved) alert("Congratulations! You solved the puzzle!");
 }
 
+
+/*   
+   showSolution()
+   Shows the solution to the Hitori puzzle
+*/
 function showSolution () {
    for (var i = 0; i < allCells.length; i++) {
       allCells[i].style.color = "";
@@ -101,6 +194,13 @@ function showSolution () {
    };   
 }
 
+
+/*
+   drawHitori(numbers, blocks, rating)
+   Returns a text string of the HTML code to
+   display a Hitori puzzle table based on the contents of
+   the numbers, blocks, and rating parameters.
+*/
 function drawHitori(numbers, blocks, rating) {
 
    /* Initial HTML String for the Hitori Puzzle */
