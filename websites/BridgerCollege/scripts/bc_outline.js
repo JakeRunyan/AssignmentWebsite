f0aeed1131689fce34b0ad85348ebfde311edd75
Jake Runyan <43076688+Jtmonkeyboy@users.noreply.github.com>
2023-11-14 12:05:12 -0700

    Added 6-3

---
+++ websites/BridgerCollege/scripts/bc_outline.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*
+
+   Author: 
+   Date:   
+
+   Filename: bc_outline.js
+
+
+   Function List
+   =============
+
+   makeOutline()
+      Generates the text of the table of contents
+      as a nested list
+
+   createList(source, TOCList, headings)
+      Creates an outline based on the source document,
+      list items are appended to TOCList,
+      the items list are based on the element names
+      specified in the headings array
+
+
+*/
+
---
+++ websites/BridgerCollege/scripts/bc_switch.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+
+   Author: 
+   Date:   
+
+   Filename: bc_switch.js
+   
+   setupStyles()
+   Function to set up the style sheet switcher and insert
+   from buttons to allow the user to switch between web
+   view and page view
+   
+*/
+
