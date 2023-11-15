2e47e53da0fc65a8f8358c20c95076af1bf24ea4
Jake Runyan <43076688+Jtmonkeyboy@users.noreply.github.com>
2023-11-15 16:54:10 -0700

    Created two buttons

--- websites/BridgerCollege/scripts/bc_switch.js
+++ websites/BridgerCollege/scripts/bc_switch.js
@@ -2,8 +2,8 @@
 
 /*
 
-   Author: 
-   Date:   
+   Author: Jake Ruyan
+   Date: 11/15
 
    Filename: bc_switch.js
    
@@ -14,3 +14,28 @@
    
 */
 
+window.addEventListener("load", setupStyles);
+
+function setupStyles() {
+   // Create a link elem for the style sheet
+   var pageStyle = document.createElement("link");
+   pageStyle.setAttribute("href", "styles/bc_page.css");
+   pageStyle.setAttribute("rel", "stylesheet");
+   pageStyle.setAttribute("disabled", "disabled");
+
+   // Append that link elem to that doc head
+   document.head.appendChild(pageStyle);
+   pageStyle.disabled = true;
+
+   // Insert buttons for the style switcher
+   var buttonDIV = document.createElement("div");
+   buttonDIV.setAttribute("id", "styleButtons");
+
+   var webButton = document.createElement("input");
+   webButton.setAttribute("type", "button");
+   webButton.setAttribute("value", "Web View")
+
+   var pageButton = document.createElement("input");
+   pageButton.setAttribute("type", "button");
+   pageButton.setAttribute("value", "Page View");
+}
