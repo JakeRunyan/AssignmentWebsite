28679ef9c954b67313724163cdcf5b5bc520657b
Jake Runyan <43076688+Jtmonkeyboy@users.noreply.github.com>
2023-11-16 16:57:33 -0700

    Adding indent to the nestedList

--- websites/AboutMe/scripts/script.js
+++ websites/AboutMe/scripts/script.js
@@ -4,3 +4,25 @@
     Author: Jake Runyan
     Date: 11/16
 */
+
+window.addEventListener("load", init);
+
+function init() {
+    // Adding the font style and my nickname
+    document.body.setAttribute("font-family", "Arial, sans-serif");
+    document.getElementById("nickname").innerHTML = "Jakey Boi";
+
+    // Creating a new li elem and getting the favorites elem
+    var list = document.createElement("li");
+    var favElem = document.getElementById("favorites");
+    favElem.appendChild(document.createElement("ol"));
+    
+    // Create an array that contains all of my favorite things and print them all into a list
+    var favs = ["Erin (my wife)", "Coding", "Learning", "My brother", "Video games"];
+    var favGames = ["Minecraft", "Call of Duty", "Fortnite", "Remnant"];
+
+    for (var i = 0; i < favs.length; i++) {
+        list.innerHTML = favs[i];
+        favElem.appendChild(list);
+    }
+}
