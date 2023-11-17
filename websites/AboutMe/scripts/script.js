db1bda3f5147ad55f27dc85c9f8b6e5564c2a9b5
Jake Runyan <43076688+Jtmonkeyboy@users.noreply.github.com>
2023-11-16 17:46:29 -0700

    Added in 3 random photos

--- websites/AboutMe/scripts/script.js
+++ websites/AboutMe/scripts/script.js
@@ -8,23 +8,43 @@
 window.addEventListener("load", init);
 
 function init() {
-    // Adding the font style and my nickname
-    document.body.setAttribute("font-family", "Arial, sans-serif");
+    // Adding the font style, my nickname and hometown
+    document.body.setAttribute("style", "font-family: Arial, sans-serif");
     document.getElementById("nickname").innerHTML = "Jakey Boi";
+    document.getElementById("hometown").innerHTML = "Cottonwood Heights";
 
     // Creating a new ol elem and getting the favorites elem
     var favElem = document.getElementById("favorites");
     var nestedList = document.createElement("ol");
+    var doubleNestedList = document.createElement("ol");
     favElem.appendChild(nestedList);
     
     // Create an array that contains all of my favorite things and print them all into a list
     var favs = ["Erin (my wife)", "Coding", "Learning", "My brother", "Video games"];
     var favGames = ["Minecraft", "Call of Duty", "Fortnite", "Remnant"];
 
+    // Print the favs list
     for (var i = 0; i < favs.length; i++) {
         // Create the new li elem each time
         var list = document.createElement("li");
+        list.setAttribute("style", "color: red");
         list.innerHTML = favs[i];
         nestedList.appendChild(list);
     }
+
+    for (var i = 0; i < favGames.length; i++) {
+        // Create the new li elem each time
+        var list = document.createElement("li");
+        list.setAttribute("style", "color: red");
+        list.innerHTML = favGames[i];
+        doubleNestedList.appendChild(list);
+    }
+    favElem.appendChild(doubleNestedList);
+
+    // Mess with images
+    var aside = document.getElementById("side");
+    var imgElem = document.createElement("img");
+    imgElem.src = "images/me" + Math.ceil(Math.random() * 3) + ".jpg";
+
+    aside.appendChild(imgElem);
 }
