040b282bbadaaa00d58dfa641b11efac250044be
Jake Runyan <43076688+Jtmonkeyboy@users.noreply.github.com>
2023-11-16 17:06:27 -0700

    Got the fav list printed

--- websites/AboutMe/scripts/script.js
+++ websites/AboutMe/scripts/script.js
@@ -12,17 +12,19 @@ function init() {
     document.body.setAttribute("font-family", "Arial, sans-serif");
     document.getElementById("nickname").innerHTML = "Jakey Boi";
 
-    // Creating a new li elem and getting the favorites elem
-    var list = document.createElement("li");
+    // Creating a new ol elem and getting the favorites elem
     var favElem = document.getElementById("favorites");
-    favElem.appendChild(document.createElement("ol"));
+    var nestedList = document.createElement("ol");
+    favElem.appendChild(nestedList);
     
     // Create an array that contains all of my favorite things and print them all into a list
     var favs = ["Erin (my wife)", "Coding", "Learning", "My brother", "Video games"];
     var favGames = ["Minecraft", "Call of Duty", "Fortnite", "Remnant"];
 
     for (var i = 0; i < favs.length; i++) {
+        // Create the new li elem each time
+        var list = document.createElement("li");
         list.innerHTML = favs[i];
-        favElem.appendChild(list);
+        nestedList.appendChild(list);
     }
 }
