e5ebc51e7cdce3f0b49be014d3d9ca3685c96962
Jake Runyan <Jake.runyan@runpycloudconsulting.com>
2023-11-28 14:14:19 -0700

    Printed payment info

--- websites/Coctura/scripts/co_payment.js
+++ websites/Coctura/scripts/co_payment.js
@@ -4,8 +4,8 @@
 
    Payment Form Script
    
-   Author: 
-   Date:   
+   Author: Jake Runyan
+   Date: 11/28/2023
    
    Filename: co_payment.js
    
@@ -40,4 +40,22 @@
       Returns true of idNum satisfies the Luhn Algorithm
 
 */
+
+window.addEventListener("load", function() {
+   var formData = location.search.slice(1);
+   formData = formData.replace(/\+/g, " "); // Replacing + with a space
+   formData = decodeURIComponent(formData); // Decodeing %24 to render $, decode %28 to (
+   var formField = formData.split(/[&=]/g);
+
+   // Write values out to form
+   document.forms.order.elements.orderDate.value = formField[1];
+   document.forms.order.elements.modelName.value = formField[5];
+   document.forms.order.elements.qty.value = formField[7];
+   document.forms.order.elements.initialCost.value = formField[9];
+   document.forms.order.elements.protectionName.value = formField[13];
+   document.forms.order.elements.protectionCost.value = formField[15];
+   document.forms.order.elements.subtotal.value = formField[17];
+   document.forms.order.elements.salesTax.value = formField[19];
+   document.forms.order.elements.totalCost.value = formField[21];
+});
 
