23654b18f89888b3dd55dc09c8af190eb30c24d4
Jake Runyan <Jake.runyan@runpycloudconsulting.com>
2023-12-11 19:40:23 -0700

    Added totalCost

--- websites/6-7Challenge/scripts/script.js
+++ websites/6-7Challenge/scripts/script.js
@@ -6,7 +6,8 @@
 
 window.addEventListener("load", function() {
     document.getElementById("weight").oninput = weight;
-    document.getElementById("days").oninput = calcCost;
+    document.getElementById("days").onchange = totalCost;
+    document.getElementById("sing").onchange = checkbox(document.getElementById("sing").value);
 });
 
 
@@ -29,14 +30,47 @@ function weight() {
 }
 
 /**
- * calcCost()
+ * calcBoardCost()
  * Determines the cost by getting the value for Days of Boarding
  */
-function calcCost() {
+function calcBoardCost() {
     var days = document.getElementById("days").value;
-    var cost = days * 50;
-    if (cost > 0)
-        document.getElementById("BoardingFee").value = cost;
-    else
-        document.getElementById("BoardingFee").value = "0";
+    var cost = 0;
+    if (days != null)
+        cost = days * 19.99;
+    if (cost > 0) {
+        document.getElementById("boardingFee").value = formatNumber(cost, 2);
+        document.getElementById("boardingCost").value = formatNumber(cost, 2);
+    }
+    else {
+        document.getElementById("boardingFee").value = "0.00";
+        document.getElementById("boardingCost").value = "0.00";
+    }
 }
+
+function totalCost() {
+    calcBoardCost();
+    calc
+    var cost = document.getElementById("totalCost").value;
+    document.getElementById("totalCost").value = formatNumber(cost, 2);
+}
+
+/**
+ * formatNumber()
+ * decimals sets the amount of decimals to display
+ */
+function formatNumber(val, decimals) {
+    return val.toLocaleString(undefined, {
+       minimumFractionDigits: decimals,
+       maximumFractionDigits: decimals
+    });
+ }
+
+ /**
+  * checkbox()
+  * This method hides or shows the different checkbox divs
+  * @param {True or false} object 
+  */
+ function checkbox(object) {
+    document.getElementById("singTitle").value = object;
+ }
